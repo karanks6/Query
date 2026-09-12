c790773b074fbbe5af2d448aacc98a2fc9b35931
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-12 11:50:52 +0530

    Update concept_dao.g.dart

---
+++ lib/data/local/daos/concept_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'concept_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$ConceptDaoMixin on DatabaseAccessor<AppDatabase> {
+  $BookmarkedConceptsTable get bookmarkedConcepts =>
+      attachedDatabase.bookmarkedConcepts;
+}
