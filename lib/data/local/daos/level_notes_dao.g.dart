e7234464f0b1dcfe6c6a83891da3aba4b2c82d26
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-14 00:46:42 +0530

    feat: Add or update level_notes_dao.g.dart for Phase 3 deep features

---
+++ lib/data/local/daos/level_notes_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'level_notes_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$LevelNotesDaoMixin on DatabaseAccessor<AppDatabase> {
+  $LevelNotesTable get levelNotes => attachedDatabase.levelNotes;
+}
