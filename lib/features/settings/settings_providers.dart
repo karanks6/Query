2fa34b6df4ab8b82d2d050efe3db0b986aab2f55
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-15 01:18:45 +0530

    fix: Remove unused import from Settings Providers

--- lib/features/settings/settings_providers.dart
+++ lib/features/settings/settings_providers.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/providers.dart';
-import 'package:collection/collection.dart';
-
+import '../../data/local/app_database.dart';
 final unlockedThemesProvider = FutureProvider<List<String>>((ref) async {
   final themesDao = ref.watch(themesDaoProvider);
   final unlocks = await themesDao.getUnlockedThemes();
