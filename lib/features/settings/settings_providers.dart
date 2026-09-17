5f95e83b4668607e481c302cce77cedfea90d89d
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-17 16:10:26 +0530

    fix(settings-providers): remove unused app_database.dart import

--- lib/features/settings/settings_providers.dart
+++ lib/features/settings/settings_providers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/providers.dart';
-import '../../data/local/app_database.dart';
 final unlockedThemesProvider = FutureProvider<List<String>>((ref) async {
   final themesDao = ref.watch(themesDaoProvider);
   final unlocks = await themesDao.getUnlockedThemes();
