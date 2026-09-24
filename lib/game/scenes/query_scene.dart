2ca934c17dd7bf4b8aa12d0a6f1ce34e2621008b
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-24 22:42:12 +0530

    refactor: migrate HasGameRef to HasGameReference in QueryScene

--- lib/game/scenes/query_scene.dart
+++ lib/game/scenes/query_scene.dart
@@ -1,9 +1,9 @@
 import 'package:flame/components.dart';
 import '../query_game.dart';
 
-abstract class QueryScene extends Component with HasGameRef<QueryGame> {
+abstract class QueryScene extends Component with HasGameReference<QueryGame> {
   Future<void> onEnter() async {}
   Future<void> onExit() async {}
   
-  List<String> get activeOverlays => [];
+  List<String> get activeOverlays => ['flutter_ui'];
 }
