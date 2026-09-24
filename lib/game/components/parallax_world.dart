f55443e2ffd3f9913b74dc303a3ee23cb0b52ca0
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-24 22:43:34 +0530

    refactor: rename gameSize to size in onGameResize

--- lib/game/components/parallax_world.dart
+++ lib/game/components/parallax_world.dart
@@ -41,9 +41,9 @@ class ParallaxWorldComponent extends PositionComponent {
   }
 
   @override
-  void onGameResize(Vector2 gameSize) {
-    super.onGameResize(gameSize);
-    size = gameSize;
+  void onGameResize(Vector2 size) {
+    super.onGameResize(size);
+    size = size;
   }
 }
 
