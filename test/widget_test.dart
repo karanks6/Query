89bca0084bd499f0f92727263d8b96d13ed6e4d0
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-26 11:55:04 +0530

    test: update widget test to mount GameRoot instead of removed QueryApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,13 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:query/main.dart';
 
 void main() {
-  testWidgets('App renders splash screen', (WidgetTester tester) async {
+  testWidgets('App renders root', (WidgetTester tester) async {
     await tester.pumpWidget(
       const ProviderScope(
-        child: QueryApp(),
+        child: GameRoot(),
       ),
     );
     expect(find.byType(ProviderScope), findsOneWidget);
-    await tester.pump(const Duration(seconds: 2)); // clear the Future.delayed in splash
   });
 }
