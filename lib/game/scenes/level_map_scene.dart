6906e69c6eedaa101ffb7f38a2d06dd349fb91bd
suvarnakaran <suvarnakaran77@gmail.com>
2026-09-26 11:54:36 +0530

    refactor: rebuild LevelMapScene as native Flame UI and fix dummy level schema

--- lib/game/scenes/level_map_scene.dart
+++ lib/game/scenes/level_map_scene.dart
@@ -2,11 +2,57 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'query_scene.dart';
 
+import '../components/ui/game_button_component.dart';
+import 'gameplay_scene.dart';
+import '../../data/content/models/level_model.dart';
+import '../../core/sandbox_engine/level_schema.dart';
 class LevelMapScene extends QueryScene {
+  final String worldId;
+
+  LevelMapScene({required this.worldId});
+
   @override
   Future<void> onLoad() async {
-    // We will draw a winding path background here
     add(WindingPathComponent());
+
+    final backBtn = GameButtonComponent(
+      title: 'BACK',
+      size: Vector2(100, 40),
+      position: Vector2(40, 60),
+      onPressed: () {
+        game.popScene();
+      },
+    );
+    add(backBtn);
+
+    final dummyLevel = LevelModel(
+      id: 'level_1',
+      worldId: worldId,
+      levelNumber: 1,
+      title: 'First Query',
+      narrative: 'Select all users.',
+      type: LevelType.puzzle,
+      schema: LevelSchema(tables: []),
+      xpReward: 50,
+      allowedWorldNumber: 1,
+      hints: const [],
+      orderSensitive: false,
+      performanceActive: false,
+      efficiencyThreshold: 0.8,
+      schemaSql: '',
+      seedSql: '',
+      expectedResult: const [],
+    );
+
+    final node = GameButtonComponent(
+      title: 'LEVEL 1',
+      size: Vector2(140, 50),
+      position: Vector2(150, 150),
+      onPressed: () {
+        game.pushScene(GameplayScene(level: dummyLevel));
+      },
+    );
+    add(node);
   }
 }
 
@@ -18,7 +64,7 @@ class WindingPathComponent extends PositionComponent {
   @override
   void onGameResize(Vector2 size) {
     super.onGameResize(size);
-    size = size;
+    this.size = size;
   }
 
   @override
